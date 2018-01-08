9c043c28c15c2bedf16ff3cb598caa0ca7d1ad9e
jmccann2000 <jakemccann2000@gmail.com>
2018-01-08 15:02:04 -0500

    Completed function but still cannot call it correctly

--- lesson000/file_reader.py
+++ lesson000/file_reader.py
@@ -5,12 +5,18 @@
 
 
 class FileReader(object):
+    '''not sure if the function should  go here'''
     '''
     This class provides the basic functionality for reading in a code file and decomposing it into its
     whitespace separated tokens.
     '''
 
     def load_tokens_from_file_path(self, file_path):
+
+        test = open(file_path,"r")
+        for line in test:
+            tokens = line.split()
+            print(tokens)
         '''
         This function reads in the contents of the file at file_path, decomposing that files contents into
         a list of tokens. A token is any continuous sequence of non-whitespace characters separated from other
@@ -20,4 +26,5 @@ class FileReader(object):
         @returns A list of tokens (strings) comprising the contents of the file
         '''
         # Implement me!
-        raise NotImplementedError()
+    '''or if function should go here'''
+    load_tokens_from_file_path(self, '/Learning-To-Code/lesson000/file_reader.py')
